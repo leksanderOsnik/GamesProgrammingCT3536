2c41fb90c5262b262157fcc2ae205cb202602ca3
leksanderOsnik <47179285+leksanderOsnik@users.noreply.github.com>
2020-10-05 15:59:35 +0100

    Assignment 1
    Assignment 1 - Rotation of Mars around its own axis and the two moons.  Camera control via arrow keys functionality

---
+++ Mars and moons/Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject marsObject;
+    public GameObject phobosObject;
+    public GameObject deimosObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera.main.transform.position = new Vector3(0, 0, -150);
+        Camera.main.transform.LookAt(marsObject.transform);
+        Rigidbody marsrb = marsObject.GetComponent<Rigidbody>();
+        marsrb.angularVelocity = new Vector3(0, 3, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        phobosObject.transform.RotateAround(marsObject.GetComponent<Transform>().position, Vector3.up, 90 * Time.deltaTime);
+        deimosObject.transform.RotateAround(marsObject.GetComponent<Transform>().position, Vector3.up, 60 * Time.deltaTime);
+        marsObject.transform.RotateAround(marsObject.GetComponent<Transform>().position, Vector3.up, -Input.GetAxis("Horizontal") * 180 * Time.deltaTime);
+        marsObject.transform.RotateAround(marsObject.GetComponent<Transform>().position, Vector3.right, -Input.GetAxis("Vertical") * 180 * Time.deltaTime);
+    }
+}
